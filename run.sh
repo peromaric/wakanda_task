04fe832a135a7df9e3085b5bdb5a1a4ebe9cbd6c
Pero Maric <peromaric91@gmail.com>
2022-08-06 12:56:30 +0300

    add contract paths to run script

--- run.sh
+++ run.sh
@@ -5,27 +5,31 @@ docker-compose -f docker-compose.yml up -d --build
 #Create a local tmp folder that contains exports
 mkdir ./tmp
 
+# PATHS
+BLOCKCHAIN_CONT_CONTRACT_PATH="/app/wakanda_blockchain/artifacts/contracts"
+API_CONT_CONTRACT_PATH="/app/wakanda_api/wakanda_api/contract_data"
+
 #Contracts deployement, save output to .ENV
 docker exec -it wakanda_blockchain_cont npx hardhat run --network localhost \
   scripts/deploy.js > ./tmp/addresses.json
 
 #Copy the contract.json to the temporary folder
 
-docker cp wakanda_blockchain_cont:/app/wakanda_blockchain/artifacts/contracts/wakandaVotingContract.sol/WakandaVotingContract.json \
+docker cp wakanda_blockchain_cont:$BLOCKCHAIN_CONT_CONTRACT_PATH/wakandaVotingContract.sol/WakandaVotingContract.json \
   ./tmp/
 
-docker cp wakanda_blockchain_cont:/app/wakanda_blockchain/artifacts/contracts/wakandaERC20.sol/WakandaERC20.json \
+docker cp wakanda_blockchain_cont:$BLOCKCHAIN_CONT_CONTRACT_PATH/wakandaERC20.sol/WakandaERC20.json \
   ./tmp/
 
 #Copy contract and address data to api cont (it waits for the data to arrive)
 docker cp ./tmp/WakandaVotingContract.json \
-  wakanda_api_cont:/app/wakanda_api/wakanda_api/contract_data/
+  wakanda_api_cont:$API_CONT_CONTRACT_PATH
 
 docker cp ./tmp/WakandaERC20.json \
-  wakanda_api_cont:/app/wakanda_api/wakanda_api/contract_data/
+  wakanda_api_cont:$API_CONT_CONTRACT_PATH
 
 docker cp ./tmp/addresses.json \
-  wakanda_api_cont:/app/wakanda_api/wakanda_api/contract_data/
+  wakanda_api_cont:$API_CONT_CONTRACT_PATH
 
 #Remove tmp folder
 rm -rf ./tmp
